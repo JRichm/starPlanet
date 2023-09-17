269871fc5597509eed8c5f416c95439e896bbae7
James Richmond <jblackenmusic@gmail.com>
2023-09-16 21:41:23 -0600

    added scripts

---
+++ Assets/Scripts/ShipClass.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipClass : MonoBehaviour
+{
+    private GameObject player;
+    
+
+    private void Start() {
+
+    }
+}
