fd10d1583d89772806ce2bbf301f4e957579993f
James Richmond <jblackenmusic@gmail.com>
2023-09-18 16:51:59 -0600

    added explosion

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class BulletScript : MonoBehaviour
 {
     [SerializeField] float bulletSpeed = 100f;
+    [SerializeField] ParticleSystem explosionEffect;
 
     private Rigidbody rb;
 
     private void Start() {
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * bulletSpeed;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        Instantiate(explosionEffect, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 }
