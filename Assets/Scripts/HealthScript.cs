5f301484ae890928a6c5a2d1bb64627431886eb9
James Richmond <jblackenmusic@gmail.com>
2023-09-20 16:49:02 -0600

    worked on health script

--- Assets/Scripts/HealthScript.cs
+++ Assets/Scripts/HealthScript.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class HealthScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float maxHealth;
+    [SerializeField] private float currentHealth;
+
+    private void Start() {
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void takeDamage(float damage) {
+        currentHealth -= damage;
+
+        if (currentHealth < 0) {
+            // death implementation
+        }
+    }
+
+    public void addHealth(float health) {
+        if (health + currentHealth > maxHealth) {
+            currentHealth = maxHealth;
+        } else {
+            currentHealth += health;
+        }
+    }
+
+    public void setHealth(float health) {
+        if (health > maxHealth) {
+            currentHealth = maxHealth;
+        } else {
+            currentHealth = health;
+        }
     }
 }
