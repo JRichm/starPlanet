6f5e2db5a2355abd057e850f7531d88d3f0a360e
James Richmond <jblackenmusic@gmail.com>
2023-10-08 11:57:44 -0600

    added homing missile

---
+++ Assets/Scripts/HomingMissileScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class HomingMissileScript : MonoBehaviour
+{
+    [SerializeField] float missleThrust;
+    [SerializeField] float targetLockSensitivity;
+
+    private void Start() {
+        
+    }
+}
