7c9f91db6837278994f19a710bae00f85cd4e06c
James Richmond <jblackenmusic@gmail.com>
2023-10-08 12:30:05 -0600

    updated homing missile

--- Assets/Scripts/HomingMissileScript.cs
+++ Assets/Scripts/HomingMissileScript.cs
@@ -8,7 +8,15 @@ public class HomingMissileScript : MonoBehaviour
     [SerializeField] float missleThrust;
     [SerializeField] float targetLockSensitivity;
 
+    private Rigidbody rb;
+    private CapsuleCollider col;    
+
     private void Start() {
-        
+        rb = GetComponent<Rigidbody>();
+        col = GetComponentInChildren<CapsuleCollider>();
+    }
+
+    private void Update() {
+        rb.AddForceAtPosition(-transform.up * missleThrust, Vector3.zero, ForceMode.Force); ;
     }
 }
